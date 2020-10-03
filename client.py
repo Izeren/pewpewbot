f43129bc46ba2174790be00b8a34a761d066f4c6
artli <artliss96@gmail.com>
2020-10-03 18:36:48 +0300

    add API client

---
+++ client.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+from functools import wraps
+
+import aiohttp
+from aiohttp import ClientError as AiohttpClientError
+from marshmallow.exceptions import ValidationError
+
+from .models import AuthenticationError, CodeSchema, CodeVerdict, Status, StatusSchema, TokenSchema
+
+
+DEFAULT_ENDPOINT = 'http://classic.dzzzr.ru'
+DEFAULT_CITY = 'moscow'
+
+
+class Urls:
+    def __init__(self, endpoint=DEFAULT_ENDPOINT, city=DEFAULT_CITY):
+        base = f'{endpoint}/{city}'
+        self.log_in = f'{base}/API/login.php'
+        self.status = f'{base}/go/'
+        self.post_code = f'{base}/go/'
+
+
+@dataclass
+class ClientConfig:
+    total_timeout: float
+    max_concurrent_connections: int
+
+
+DEFAULT_URLS = Urls()
+DEFAULT_CONFIG = ClientConfig(total_timeout=20, max_concurrent_connections=5)
+
+
+class ClientError(Exception):
+    pass
+
+
+def _wrap_errors(wrapped):
+    @wraps(wrapped)
+    async def wrapper(*args, **kwargs):
+        try:
+            return wrapped(*args, **kwargs)
+        except (AuthenticationError, AiohttpClientError, ValidationError) as err:
+            raise ClientError('API client failed') from err
+    return wrapper
+
+
+class Client:
+    def __init__(self, urls: Urls = DEFAULT_URLS, config: ClientConfig = DEFAULT_CONFIG):
+        self._urls = urls
+        self._client = aiohttp.ClientSession(
+            connector=aiohttp.BaseConnector(limit=config.max_concurrent_connections),
+            timeout=config.total_timeout,
+            raise_for_status=True)
+        self._entered = False
+        self._token = None
+
+    @property
+    def token(self):
+        if self._token is None:
+            raise AuthenticationError('Not logged in')
+        return self._token
+
+    async def __aenter__(self):
+        await self._client.__aenter__()
+        self._entered = True
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self._entered = False
+        await self._client.__aexit__(exc_type, exc_val, exc_tb)
+
+    async def _request(self, method, url, **kwargs):
+        if not self._entered:
+            raise ValueError(
+                'The Client object should be used as an asynchronous context manager ("async with Client(...)")')
+        async with self._client.request(method, url, **kwargs) as resp:
+            return await resp.json(encoding='utf-8')
+
+    @_wrap_errors
+    async def log_in(self, login, password):
+        resp = await self._request('get', self._urls.log_in, params=dict(login=login, password=password))
+        self._token = TokenSchema().load(resp)
+
+    @_wrap_errors
+    async def status(self) -> Status:
+        resp = await self._request(
+            'get', self._urls.status, params=dict(
+                s=self.token,
+                api='true'))
+        return StatusSchema().load(resp)
+
+    @_wrap_errors
+    async def post_code(self, code: str) -> CodeVerdict:
+        resp = await self._request(
+            'post', self._urls.status + '?api=true', data=dict(
+                s=self.token,
+                action='entcod',
+                cod=code))
+        return CodeSchema().load(resp)
---
+++ models.py
@@ -0,0 +1,160 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, List
+
+from marshmallow import Schema, fields, post_load
+from marshmallow_enum import EnumField
+
+
+def _rename(data, **new_names):
+    new_fields = {}
+    for new_name, old_name in new_names.items():
+        if old_name in data:
+            new_fields[new_name] = data[old_name]
+            del data[old_name]
+    return {**data, **new_fields}
+
+
+class AuthenticationError(ValueError):
+    pass
+
+
+class StatusError(Enum):
+    SUCCESS = 1
+
+
+class CodeVerdict(Enum):
+    SUCCESS = 1
+    FAILURE = 2
+
+
+@dataclass
+class LevelStatus:
+    isSabotage: bool
+    levelNumber: int
+    noMasterCode: bool
+    isBonusLevel: bool
+    bonusLevelTime: int
+    hint1: str
+    hint2: str
+    hint3: str
+    neededCodes: int
+    codesFounded: int
+    totalCodes: int
+    bonusCodesTotal: int
+    bonusCodesFounded: int
+    skvoz: bool
+    bonusCodesTimes: Any
+    koline: str
+    question: str
+    locationComment: str
+    spoilers: List[str]
+    koinspoiler: bool
+    timeOnLevel: str
+    tm: int
+    takeBreak: bool
+
+    @classmethod
+    def from_api(cls, data) -> 'LevelStatus':
+        return cls(**_rename(data))
+
+
+@dataclass
+class Status:
+    game_id: str
+    game_title: str
+    game_greeting: str
+    game_legend: str
+
+    game_start_time: str
+    hold_time: str
+    break_time: str
+    current_time: str
+
+    current_level: LevelStatus
+    bonus_levels: List[LevelStatus]
+
+    last_organizational_message: str
+    messages: List[str]
+    error: StatusError
+    error_text: str
+
+    @classmethod
+    def from_api(cls, data) -> 'Status':
+        return cls(**_rename(
+            data, game_id='gameId', game_title='gameName', game_greeting='greeting', game_legeng='legend',
+            game_start_time='gameStartTime', hold_time='holdTime', break_time='onBreak', current_time='currentTime',
+            current_level='level', bonus_levels='bonusLevels', last_organizational_message='lastOrgMessage',
+            error='errNo', error_text='errText'))
+
+
+class LevelStatusSchema(Schema):
+    isSabotage = fields.Bool()
+    levelNumber = fields.Int()
+    noMasterCode = fields.Bool()
+    isBonusLevel = fields.Bool()
+    bonusLevelTime = fields.Int()
+    hint1 = fields.Str()
+    hint2 = fields.Str()
+    hint3 = fields.Str()
+    neededCodes = fields.Int()
+    codesFounded = fields.Int()
+    totalCodes = fields.Int(required=True)
+    bonusCodesTotal = fields.Int(required=True)
+    bonusCodesFounded = fields.Int(required=True)
+    skvoz = fields.Bool(required=True)
+    bonusCodesTimes = fields.Raw(required=True)
+    koline = fields.Str(required=True)
+    question = fields.Str(required=True)
+    locationComment = fields.Str(required=True)
+    spoilers = fields.List(fields.Str, required=True)
+    koinspoiler = fields.Bool(required=True)
+    timeOnLevel = fields.Str(required=True)
+    tm = fields.Int(required=True)
+    takeBreak = fields.Bool(required=True)
+
+    @post_load
+    def to_object(self, data) -> LevelStatus:
+        return LevelStatus.from_api(data)
+
+
+class StatusSchema(Schema):
+    gameName = fields.Str(required=True)
+    greeting = fields.Str(required=True)
+    lastOrgMessage = fields.Str(required=True)
+    gameId = fields.Str(required=True)
+    gameStartTime = fields.Str()
+    holdTime = fields.Str()
+    onBreak = fields.Str()
+    currentTime = fields.Str()
+    legend = fields.Str()
+    level = fields.Nested(LevelStatusSchema)
+    bonusLevels = fields.List(fields.Nested(LevelStatusSchema))
+    messages = fields.List(fields.Str)
+    errNo = EnumField(StatusError, required=True, by_value=True)
+    errText = fields.Str()
+
+    @post_load
+    def to_object(self, data) -> Status:
+        return Status.from_api(data)
+
+
+class TokenSchema(Schema):
+    error = fields.Str(required=True)
+    code = fields.Int(required=True)
+    userName = fields.Str()
+
+    userToken = fields.Str()
+    @post_load
+    def extract_token(self, data) -> str:
+        if data['code'] != 2:
+            raise AuthenticationError(data)
+        return data['userToken']
+
+
+class CodeSchema(Schema):
+    err = EnumField(CodeVerdict, required=True, by_value=True)
+
+    @post_load
+    def parse_error(self, data) -> CodeVerdict:
+        return data['err']
