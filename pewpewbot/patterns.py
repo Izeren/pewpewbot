4a461998b4e9a26838c9952ad7940024da3d8b3f
Izeren <yushalnik@bk.ru>
2021-11-16 19:41:26 +0000

    Extend standard pattern with word123 (cyrillic letters+digits) pattern (#51)

--- pewpewbot/patterns.py
+++ pewpewbot/patterns.py
@@ -3,8 +3,11 @@
 
 # Default pattern for parsing coordinates from telegram chat
 STANDARD_COORDS_PATTERN = r'(\d{2}[\.,]\d{3,})'
+# Cyrillic DR pattern. To be automatically mapped into latin DR
+CYRILLIC_DR_PATTERN = r'\d*[др]\d*[др]\d*'
 # Default pattern for parsing standard dozor code from telegram chat
-STANDARD_CODE_PATTERN = r'\d*[dr]\d*[dr]\d*|\d*[др]\d*[др]\d*'
+# supports classic dr codes, cyrillic dr codes and word123 format
+STANDARD_CODE_PATTERN = r'\d*[dr]\d*[dr]\d*|\d*[др]\d*[др]\d*|[ёа-я]+[0-9]+'
 # Pattern for command with forced code
 FORCED_CODE_PATTERN = r'^/ '
 # Pattern for Level Scheme link parsing
